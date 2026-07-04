30fa44e85b2860685c72f6952732e9b5ceebe355
vijay kumar Yagavinti <yvijaykumar200210@gmail.com>
2026-07-04 23:21:51 +0530

    added javascript file

---
+++ simpleSum.js
@@ -0,0 +1,4 @@
+a = 10
+b = 20
+sum = a + b
+console.log("This is the first JavaScript program: " + sum)
---
+++ variables.js
@@ -0,0 +1,40 @@
+// var keyword
+
+var name = 10;
+var name = "kumar";
+console.log(name);
+
+// let keyword
+
+let name1 = "mahesh";
+// let name1 = "kumar";
+console.log(name1);
+
+// const keyword
+
+const name2 = "mahesh";
+// name2 = "kumar";
+console.log(name2);
+
+// declaration
+var a;
+// var a // you can redeclare variable 'a' using var keyword
+let b;
+// let b; // error: cannot redeclare variable 'b'
+// const c;
+
+// assign or initialize
+a = 10;
+b = "mahesh";
+const c = "kumar";
+// c = 1234   // error: cannot reassign a constant variable 
+
+//null keyword
+var name3 = null;
+console.log(name3);
+
+
+
+let name4 = "mahesh gadu";
+let name5 = "kumar gadu";
+console.log(name4 + " java script nerchukunta vunnadu with " + name5);
